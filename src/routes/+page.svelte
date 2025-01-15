f83aca24d46f5e9726a87d37df15f250da587aa1
Vasilybib1 <vasilyrubtsovru@gmail.com>
2025-01-15 15:03:08 -0800

    learnt how to use props to pass data into components

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,10 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script>
+  import Test from './components/Test.svelte'
+  import Bar from './components/Bar.svelte'
+</script>
+
+<Bar title="Bar Title One" />
+
+<Test />
+
+<Bar title="Bar Title Two" />
---
+++ src/routes/components/Bar.svelte
@@ -0,0 +1,22 @@
+<script>
+  import Test from './Test.svelte'
+  let props = $props();
+</script>
+
+<style>
+  div{
+    background-color : black;
+    padding-left : 5px;
+    padding-right : 5px;
+    padding-bottom : 0px;
+    padding-top : 0px;
+  }
+
+  h1{
+    color : white;
+  }
+</style>
+
+<div>
+  <h1>{props.title}</h1>
+</div>
